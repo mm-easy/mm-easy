a8cdb51df768e5fdf776ff95a0cbb53c7f17257a
C1oudys <hymin310@gmail.com>
2024-04-01 16:32:35 +0900

    [fix] router 에러 수정중

--- src/app/(non-auth)/signup/page.tsx
+++ src/app/(non-auth)/signup/page.tsx
@@ -3,14 +3,12 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { useSignUp } from '@/hooks/useAuth';
 import { toast } from 'react-toastify';
-import { useRouter } from 'next/router';
 
 const SignUpPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const { signUp } = useSignUp(); 
-  const router = useRouter();
 
   const handleSignUp = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -21,7 +19,6 @@ const SignUpPage = () => {
     try {
       await signUp(email, password);
       toast.success('회원가입에 성공했습니다!'); 
-      router.push('/login');
     } catch (error) {
     }
   };
