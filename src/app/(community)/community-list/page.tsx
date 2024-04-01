e0ff8dfe468a903f2446206d07f4f70757c70d4e
Seoweol <ahddl622@naver.com>
2024-04-01 16:43:05 +0900

    [script] server-only 패키지 설치

--- src/app/(community)/community-list/page.tsx
+++ src/app/(community)/community-list/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/utils/supabase/create-client';
 
 const page = async () => {
   const supabase = createClient();
-  const { data: posts, error } = await supabase.from('posts').select('*');
+  const { data: posts, error } = await supabase.from('').select('*');
 
   console.log('posts', posts);
 
--- src/utils/supabase/create-client.ts
+++ src/utils/supabase/create-client.ts
@@ -1,5 +1,4 @@
-// import server-only
-
+import 'server-only'
 import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 export const createClient = () => {
   return createSupabaseClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET_KEY!, {});
