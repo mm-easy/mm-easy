50365972082dc8f9995fa857fe3f7c9f572c759c
Seoweol <ahddl622@naver.com>
2024-04-02 12:53:37 +0900

    [feat] 커뮤니티 메뉴 기본 폼 생성

---
+++ src/app/(community)/(components)/CommunityMenu.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+const CommunityMenu = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <button>전체</button>
+        </li>
+        <li>
+          <button>공지</button>
+        </li>
+        <li>
+          <button>질문</button>
+        </li>
+        <li>
+          <button>잡담</button>
+        </li>
+        <li>
+          <button>공부</button>
+        </li>
+        <li>
+          <button>일기</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default CommunityMenu;
--- src/app/(community)/community-list/page.tsx
+++ src/app/(community)/community-list/page.tsx
@@ -1,8 +1,10 @@
 import CommunityForm from '@/app/(community)/(components)/CommunityForm'
+import CommunityMenu from '../(components)/CommunityMenu'
 
 const CommunityPage = () => {
   return (
     <div>
+      <CommunityMenu />
       <CommunityForm />
     </div>
   )
