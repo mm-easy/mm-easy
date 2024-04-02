74f464f06331a6dfaa34a715c3ce99f5f082cc71
kimsohyeon <aotoyae@gmail.com>
2024-04-02 17:25:37 +0900

    [feat] 선택지 2개까지 삭제되도록, 2갠데 삭제 버튼 클릭하면 toast 알림

--- src/app/(quiz)/(components)/QuestionForm.tsx
+++ src/app/(quiz)/(components)/QuestionForm.tsx
@@ -54,6 +54,21 @@ const QuestionForm = ({
     }
   };
 
+  /** 문제 선택지 삭제 핸들러 */
+  const handleDeleteOption = (id: string, options: Option[], optionId: string) => {
+    if (options.length > 2) {
+      setQuestions((prev) =>
+        prev.map((question) => {
+          return question.id === id
+            ? { ...question, options: options.filter((option) => option.id !== optionId) }
+            : question;
+        })
+      );
+    } else {
+      toast.warn('최소 2개의 선택지가 있어야 합니다.');
+    }
+  };
+
   /** 문제 주관식 정답 입력 핸들러 */
   const handleChangeCorrectAnswer = (id: string, correctAnswer: string) => {
     setQuestions((prev) =>
@@ -72,93 +87,93 @@ const QuestionForm = ({
         return newQuestions;
       });
     } else {
-      alert('최소 1개의 문제는 있어야 합니다.');
-      return;
+      toast.warn('최소 1개의 문제가 있어야 합니다.');
     }
   };
 
   return (
-    <>
-      <article style={{ border: '1px solid red', margin: '10px', padding: '10px' }}>
-        {questions.map((question) => {
-          const { id, type, options } = question;
-          return (
-            /** 유형, 휴지통 섹션 */
-            <section key={id}>
-              <section style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <section>
-                  <label>
-                    <input
-                      type="radio"
-                      name={id}
-                      defaultChecked
-                      onChange={() => handleChangeType(id, QuestionType.objective)}
-                    />
-                    객관식
-                  </label>
-                  <label>
-                    <input type="radio" name={id} onChange={() => handleChangeType(id, QuestionType.subjective)} />
-                    주관식
-                  </label>
-                </section>
-                <button type="button" onClick={() => handleDeleteQuestion(id)}>
-                  🗑️
-                </button>
-              </section>
-              {/* 이미지, input 섹션 */}
+    <article style={{ border: '1px solid red', margin: '10px', padding: '10px' }}>
+      {questions.map((question) => {
+        const { id, type, options } = question;
+        return (
+          /** 유형, 휴지통 섹션 */
+          <section key={id} style={{ paddingBottom: '20px' }}>
+            <section style={{ display: 'flex', justifyContent: 'space-between' }}>
               <section>
-                {type === QuestionType.objective ? (
-                  <div>
-                    <Image src="https://via.placeholder.com/200x150" alt="fake image" width={200} height={150} />
-                    <input
-                      type="text"
-                      style={{ width: '500px' }}
-                      placeholder="문제를 입력해 주세요. ex)Apple의 한국어 뜻으로 알맞은 것은?"
-                      onChange={(e) => {
-                        e.preventDefault();
-                        handleChangeTitle(id, e.target.value);
-                      }}
-                    />
-                    {options.map((option) => {
-                      return (
-                        <div key={option.id}>
-                          <input type="text" style={{ width: '500px' }} placeholder="선택지를 입력해 주세요." />
-                        </div>
-                      );
-                    })}
-                  </div>
-                ) : (
-                  <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <Image src="https://via.placeholder.com/200x150" alt="fake image" width={200} height={150} />
-                    <input
-                      type="text"
-                      style={{ width: '500px' }}
-                      placeholder="문제를 입력해 주세요. ex)Apple의 한국어 뜻으로 알맞은 것은?"
-                      onChange={(e) => {
-                        e.preventDefault();
-                        handleChangeTitle(id, e.target.value);
-                      }}
-                    />
-                    <input
-                      type="text"
-                      style={{ width: '500px' }}
-                      placeholder="정답을 입력해 주세요."
-                      onChange={(e) => {
-                        e.preventDefault();
-                        handleChangeCorrectAnswer(id, e.target.value);
-                      }}
-                    />
-                  </div>
-                )}
-                <button type="button" onClick={() => handleAddOption(id, options)}>
-                  ➕
-                </button>
+                <label>
+                  <input
+                    type="radio"
+                    name={id}
+                    defaultChecked
+                    onChange={() => handleChangeType(id, QuestionType.objective)}
+                  />
+                  객관식
+                </label>
+                <label>
+                  <input type="radio" name={id} onChange={() => handleChangeType(id, QuestionType.subjective)} />
+                  주관식
+                </label>
               </section>
+              <button type="button" onClick={() => handleDeleteQuestion(id)}>
+                🗑️
+              </button>
             </section>
-          );
-        })}
-      </article>
-    </>
+            {/* 이미지, input 섹션 */}
+            <section>
+              {type === QuestionType.objective ? (
+                <div>
+                  <Image src="https://via.placeholder.com/200x150" alt="fake image" width={200} height={150} />
+                  <input
+                    type="text"
+                    style={{ width: '500px', fontWeight: 'bold' }}
+                    placeholder="문제를 입력해 주세요. ex)Apple의 한국어 뜻으로 알맞은 것은?"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      handleChangeTitle(id, e.target.value);
+                    }}
+                  />
+                  {options.map((option) => {
+                    return (
+                      <div key={option.id}>
+                        <input type="text" style={{ width: '500px' }} placeholder="선택지를 입력해 주세요." />
+                        <button type="button" onClick={() => handleDeleteOption(id, options, option.id)}>
+                          ❎
+                        </button>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <Image src="https://via.placeholder.com/200x150" alt="fake image" width={200} height={150} />
+                  <input
+                    type="text"
+                    style={{ width: '500px', fontWeight: 'bold' }}
+                    placeholder="문제를 입력해 주세요. ex)Apple의 한국어 뜻으로 알맞은 것은?"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      handleChangeTitle(id, e.target.value);
+                    }}
+                  />
+                  <input
+                    type="text"
+                    style={{ width: '500px' }}
+                    placeholder="정답을 입력해 주세요."
+                    onChange={(e) => {
+                      e.preventDefault();
+                      handleChangeCorrectAnswer(id, e.target.value);
+                    }}
+                  />
+                </div>
+              )}
+              <button type="button" onClick={() => handleAddOption(id, options)}>
+                ➕
+              </button>
+            </section>
+          </section>
+        );
+      })}
+    </article>
   );
 };
 
