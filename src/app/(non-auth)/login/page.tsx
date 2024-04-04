e89d75cd46c1e9ffc7f4d625b5a229a843ed5ba5
C1oudys <hymin310@gmail.com>
2024-04-04 22:23:39 +0900

    [refactor] 코드 수정

--- src/app/(non-auth)/login/page.tsx
+++ src/app/(non-auth)/login/page.tsx
@@ -25,30 +25,6 @@ const LoginPage = () => {
 
   const handleGoogleSignIn = async () => {
     await signInWithGoogle();
-    const userData = localStorage.getItem('sb-icnlbuaakhminucvvzcj-auth-token');
-    if (userData) {
-      const userdata = JSON.parse(userData);
-      const user = userdata.user; 
-
-      if (user) {
-        const nickname = user.email.split('@')[0];
-        try {
-          const { error } = await supabase
-            .from('profiles')
-            .insert([
-              { id: user.id, email: user.email, nickname, avatar_img_url: "https://via.placeholder.com/150" }
-            ]);
-  
-          if (error) {
-            console.error('프로필 정보 저장 실패:', error.message);
-          } else {
-            console.log('프로필 정보가 성공적으로 저장되었습니다.');
-          }
-        } catch (error) {
-          console.error('프로필 정보 저장 중 오류 발생:', error);
-        }
-      }
-    } 
   };
   
   const handleKakaoSignIn = async () => {
