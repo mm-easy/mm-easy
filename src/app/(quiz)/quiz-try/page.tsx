3d2565b6570b0253ef9c5db1216b74d1fff2351a
kimsohyeon <aotoyae@gmail.com>
2024-04-05 17:15:14 +0900

    [feat] quiz-try 레이아웃 구성 중

--- src/app/(quiz)/quiz-try/page.tsx
+++ src/app/(quiz)/quiz-try/page.tsx
@@ -1,9 +1,35 @@
 import SubHeader from '@/components/common/SubHeader';
+import Image from 'next/image';
 
 const QuizTryPage = () => {
   return (
     <>
       <SubHeader text="퀴즈 풀기" />
+      <main className="grid grid-cols-[20%_80%]">
+        <article className="border-solid border-r-2 border-pointColor1">
+          <section>
+            <Image
+              src="https://via.placeholder.com/144x144"
+              alt="샘플 이미지"
+              width={144}
+              height={144}
+              className="w-full"
+            />
+            <section className="p-4 flex flex-col gap-3 border-solid border-y-2 border-pointColor1">
+              <div>
+                <h4>작성자</h4>
+                <p>HaleyLove&lt;3</p>
+              </div>
+              <div>
+                <h4>등록일</h4>
+                <p>2024-04-05</p>
+              </div>
+            </section>
+          </section>
+          <p className="">English to Korean</p>
+        </article>
+        <article className=""></article>
+      </main>
     </>
   );
 };
