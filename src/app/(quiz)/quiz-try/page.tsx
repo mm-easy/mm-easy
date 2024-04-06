ee9a8593f5e58c3fcb80649202f5e1d842d7a33d
kimsohyeon <aotoyae@gmail.com>
2024-04-06 16:40:14 +0900

    [style] 퀴즈 풀기 페이지 큰 레이아웃 마무리

--- src/app/(quiz)/quiz-try/page.tsx
+++ src/app/(quiz)/quiz-try/page.tsx
@@ -6,7 +6,7 @@ const QuizTryPage = () => {
     <>
       <SubHeader text="퀴즈 풀기" />
       <main className="grid grid-cols-[10%_90%]">
-        <article className="h-[76vh] border-solid border-r-2 border-pointColor1">
+        <article className="h-[76vh] bg-bgColor1 text-pointColor1 border-solid border-r-2 border-pointColor1">
           <section>
             <Image
               src="https://via.placeholder.com/144x144"
@@ -26,9 +26,20 @@ const QuizTryPage = () => {
               </div>
             </section>
           </section>
-          <p className="">English to Korean</p>
+          <p className="p-4">English to Korean</p>
         </article>
-        <article className=""></article>
+        <article className="">
+          <header className="h-[8vh] leading-[7.5vh] flex border-solid border-b-2 border-pointColor1">
+            <h2 className="w-[10%] text-center text-pointColor1 bg-bgColor1 border-solid border-r-2 border-pointColor1">
+              난이도
+            </h2>
+            <h3 className="w-[10%] text-center border-solid border-r-2 border-pointColor1">순한맛</h3>
+            <h2 className="w-[10%] text-center text-pointColor1 bg-bgColor1 border-solid border-r-2 border-pointColor1">
+              제목
+            </h2>
+            <h3 className="pl-[3%]">Find correct words</h3>
+          </header>
+        </article>
       </main>
     </>
   );
