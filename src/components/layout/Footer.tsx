b70754bb1232f927dedba9ad597769910cb22e70
kimsohyeon <aotoyae@gmail.com>
2024-04-06 16:54:07 +0900

    [style] header 중복 padding 제거, footer leading 말고 items-center 로 세로 중앙 정렬

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <section className="h-[8vh] px-4 flex justify-between text-center leading-[7.5vh] text-sm text-white bg-pointColor1">
+    <section className="h-[8vh] px-4 flex justify-between items-center text-center text-sm text-white bg-pointColor1">
       <p>뭔말이지? Project. All rights reserved @ Coding zizon</p>
       <div className="flex gap-4">
         <Link href="https://github.com/mm-easy/mm-easy" target="\_blank">
