788eb73b32273cf2de09c325f7d12e0572999c38
redberry0217 <redberry0217@gmail.com>
2024-04-08 16:55:04 +0900

    [feat] 레벨 선택 시 레벨 카드 위치 고정

--- src/app/(quiz)/(components)/SelectQuizLevel.tsx
+++ src/app/(quiz)/(components)/SelectQuizLevel.tsx
@@ -5,16 +5,17 @@ import QuizList from './QuizList';
 import Level1 from '@/assets/level1.png';
 import Level2 from '@/assets/level2.png';
 import Level3 from '@/assets/level3.png';
-import { BlueButton } from '@/components/common/FormButtons';
+import { WhiteButton } from '@/components/common/FormButtons';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { getQuizzes } from '@/api/quizzes';
 
 import type { Quiz } from '@/types/quizzes';
-import { useState } from 'react';
 
 const SelectQuizLevel = () => {
   const [quizLevelSelected, setQuizLevelSelected] = useState<Quiz[]>([]);
+  const [selectedLevel, setSelectedLevel] = useState<number | null>(null);
   const router = useRouter();
 
   /** 퀴즈 만들기 버튼 클릭 시 */
@@ -45,21 +46,25 @@ const SelectQuizLevel = () => {
   const handleSelectLevel = (level: number) => {
     const filteredQuizzes = data.filter((item) => item.level === level);
     setQuizLevelSelected(filteredQuizzes);
+    setSelectedLevel(level);
   };
 
   return (
     <>
-      <main className="w-full bg-pointColor1 border-b-2 border-pointColor1 flex flex-col justify-center items-center">
+      <main className="w-full bg-bgColor2 border-b-2 border-pointColor1 flex flex-col justify-center items-center">
+        <div className="mt-5 text-pointColor1 text-2xl font-bold absolute top-20">난이도를 골라보세요!</div>
         <div className="mt-5 mr-1/4 absolute top-20 left-3/4">
-          <BlueButton text="퀴즈 만들기" onClick={handleMakeQuizBtn} width="w-36" />
+          <WhiteButton text="퀴즈 만들기" onClick={handleMakeQuizBtn} width="w-36" />
         </div>
-        <div className="flex items-end overflow-hidden">
+        <div className="flex items-end overflow-hidden mt-20">
           <Image
             src={Level1}
             alt="초급"
             width={350}
             height={240}
-            className="object-none transform translate-y-[50%] hover:translate-y-[10%] transition-transform duration-500 ease-in-out"
+            className={`object-none transform transition-transform duration-500 ease-in-out ${
+              selectedLevel === 1 ? 'translate-y-[10%] z-10' : 'z-0 translate-y-[50%] hover:translate-y-[10%]'
+            }`}
             onClick={() => handleSelectLevel(1)}
           />
           <Image
@@ -67,7 +72,9 @@ const SelectQuizLevel = () => {
             alt="중급"
             width={350}
             height={240}
-            className="object-none transform translate-y-[30%] hover:translate-y-[10%] transition-transform duration-500 ease-in-out"
+            className={`object-none transform transition-transform duration-500 ease-in-out ${
+              selectedLevel === 2 ? 'translate-y-[10%] z-10' : 'z-0 translate-y-[30%] hover:translate-y-[10%]'
+            }`}
             onClick={() => handleSelectLevel(2)}
           />
           <Image
@@ -75,7 +82,9 @@ const SelectQuizLevel = () => {
             alt="고급"
             width={350}
             height={240}
-            className="object-none transform translate-y-[60%] hover:translate-y-[10%] transition-transform duration-500 ease-in-out"
+            className={`object-none transform transition-transform duration-500 ease-in-out ${
+              selectedLevel === 3 ? 'translate-y-[10%] z-10' : 'z-0 translate-y-[60%] hover:translate-y-[10%]'
+            }`}
             onClick={() => handleSelectLevel(3)}
           />
         </div>
