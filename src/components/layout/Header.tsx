463995c97fd1df1114a9f4ddf9576840794ffe96
kimsohyeon <aotoyae@gmail.com>
2024-04-09 12:25:42 +0900

    [fix] 헤더 메뉴 다시 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,15 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useAuth } from '@/hooks/useAuth';
 import Link from 'next/link';
+import { useEffect } from 'react';
 import { toast } from 'react-toastify';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import MenuPage from '@/app/menu/page';
+import { useAuth } from '@/hooks/useAuth';
 import { useAtom } from 'jotai';
-import { isMenuOpenAtom } from '../../store/store';
-import { isLoggedInAtom } from '../../store/store';
-import { supabase } from '@/utils/supabase/supabase';
 import { AuthChangeEvent } from '@supabase/supabase-js';
+// import { GiHamburgerMenu } from 'react-icons/gi';
+import { isLoggedInAtom, isMenuOpenAtom } from '../../store/store';
+import { supabase } from '@/utils/supabase/supabase';
+// import MenuPage from '@/app/menu/page';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useAtom(isMenuOpenAtom);
@@ -32,7 +31,7 @@ const Header = () => {
       if (!isLoggedIn) {
         return;
       }
-      const { data, error } = await supabase.auth.getUser();  
+      const { data, error } = await supabase.auth.getUser();
       if (error) {
       } else {
         if (data) {
@@ -56,35 +55,40 @@ const Header = () => {
     toast.success('로그아웃되었습니다.');
   };
 
-  const toggleMenuModal = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  // const toggleMenuModal = () => {
+  //   setIsMenuOpen(!isMenuOpen);
+  // };
 
-  const handleLinkClick = () => {
-    setIsMenuOpen(false);
-  };
+  // const handleLinkClick = () => {
+  //   setIsMenuOpen(false);
+  // };
 
   return (
     <>
-      <section className="w-full h-[8vh] px-10 flex justify-between items-center bg-bgColor1 border-solid border-b-2 border-pointColor1">
-        <button onClick={toggleMenuModal}>
+      <header className="w-full h-[8vh] leading-[7.5vh] flex text-pointColor1 font-bold bg-bgColor1 border-solid border-b-2 border-pointColor1">
+        {/* <button onClick={toggleMenuModal}>
           <GiHamburgerMenu className="text-pointColor1" />
-        </button>
-        <Link href="/" onClick={handleLinkClick} className="text-pointColor1 font-bold">
-          로고ㅎ
+        </button> */}
+        <Link href="/" className="w-[10%] text-center">
+          LOGO
         </Link>
-        {isLoggedIn ? (
-          <button onClick={handleLogout} className="ml-10 text-pointColor1 font-bold">
-            로그아웃
-          </button>
-        ) : (
-          <Link href="/login" onClick={handleLinkClick}>
-            <button className="ml-10 text-pointColor1 font-bold">로그인</button>
-          </Link>
-        )}
-      </section>
-
-      {isMenuOpen && <MenuPage />}
+        <section className="w-[90%] flex justify-between px-10">
+          <nav className="flex gap-14">
+            <Link href="/quiz-list">퀴즈</Link>
+            <Link href="/typing-game">타자 연습</Link>
+            <Link href="/community-list">커뮤니티</Link>
+            <Link href="/about">서비스 소개</Link>
+          </nav>
+          {isLoggedIn ? (
+            <button onClick={handleLogout}>로그아웃</button>
+          ) : (
+            <Link href="/login">
+              <button>로그인</button>
+            </Link>
+          )}
+        </section>
+      </header>
+      {/* {isMenuOpen && <MenuPage />} */}
     </>
   );
 };
