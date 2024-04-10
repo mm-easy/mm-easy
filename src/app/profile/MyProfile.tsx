e44df20f35ad3b29ca106691bf6354d9847b3915
redberry0217 <redberry0217@gmail.com>
2024-04-11 02:45:28 +0900

    [feat] 로그인 유저의 정보 불러오기

--- src/app/profile/MyProfile.tsx
+++ src/app/profile/MyProfile.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import TestImg from '@/assets/level1.png';
 import Image from 'next/image';
 
-const MyProfile = () => {
+import type { User } from '@/types/users';
+
+const MyProfile = ({ currentUser }: { currentUser: User }) => {
   return (
     <main className="w-full h-full flex flex-col items-center">
       <div className="mt-10 text-center text-pointColor1 text-xl font-semibold">프로필</div>
       <div className="flex gap-20 mt-10">
         <div className="w-[250px] h-[250px]">
           <Image
-            src={TestImg}
-            alt=""
+            src={currentUser.avatar_img_url}
+            alt="사용자 프로필"
             width={250}
             height={250}
             className="w-full h-full object-cover border-solid border border-pointColor1 rounded-full"
@@ -19,11 +19,11 @@ const MyProfile = () => {
         <div className="flex flex-col gap-5 justify-between">
           <div className="flex flex-col gap-1">
             <p className="text-pointColor1 font-semibold">닉네임</p>
-            <p className="text-lg">진짜닉네임</p>
+            <p className="text-lg">{currentUser.nickname}</p>
           </div>
           <div className="flex flex-col gap-1">
             <p className="text-pointColor1 font-semibold">이메일</p>
-            <p className="text-lg">cocoa@naver.com</p>
+            <p className="text-lg">{currentUser.email}</p>
           </div>
           <div className="mt-9 text-pointColor1 underline underline-offset-4">프로필 수정</div>
         </div>
