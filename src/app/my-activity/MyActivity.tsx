c7e96a6259739aa56f8c4bc95520b26e70bef4c9
Seoweol <ahddl622@naver.com>
2024-04-11 20:00:18 +0900

    [feat] my-activity 내가 쓴 글 가져오기

--- src/app/my-activity/MyActivity.tsx
+++ src/app/my-activity/MyActivity.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { getMyActivityPosts } from '@/api/posts';
 import { fetchUserQuizzes, getQuiz } from '@/api/quizzes';
 import { useAuth } from '@/hooks/useAuth';
+import { Post } from '@/types/posts';
 import { Quiz } from '@/types/quizzes';
 import { formatToLocaleDateTimeString } from '@/utils/date';
 import { supabase } from '@/utils/supabase/supabase';
@@ -10,7 +12,9 @@ import { useEffect, useState } from 'react';
 const MyActivity = () => {
   const { getCurrentUserProfile } = useAuth();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [quizzes, setQuizzes] = useState<Quiz[]>([]); // 퀴즈 목록을 위한 상태
+  const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+  const [myPosts, setMyPosts] = useState<Post[]>([]);
+  const [activeTab, setActiveTab] = useState('quizzes'); // 활성 탭 상태
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,7 +41,6 @@ const MyActivity = () => {
           if (userProfile && userProfile.email) {
             const fetchedQuizzes = await fetchUserQuizzes(userProfile.email);
             setQuizzes(fetchedQuizzes || []);
-            console.log(fetchedQuizzes)
           }
         } catch (error) {
           console.error('퀴즈 불러오기 실패:', error);
@@ -45,8 +48,30 @@ const MyActivity = () => {
       }
     };
 
-    fetchQuizzes();
-  }, [isLoggedIn]);
+    if (activeTab === 'quizzes') {
+      fetchQuizzes();
+    }
+  }, [isLoggedIn, activeTab]);
+
+  useEffect(() => {
+    const fetchMyPosts = async () => {
+      if (isLoggedIn) {
+        try {
+          const userProfile = await getCurrentUserProfile();
+          if (userProfile && userProfile.id) {
+            const myPosts = await getMyActivityPosts(userProfile.id);
+            setMyPosts(myPosts || []);
+          }
+        } catch (error) {
+          console.error('내 글 불러오기 실패:', error);
+        }
+      }
+    };
+
+    if (activeTab === 'posts') {
+      fetchMyPosts();
+    }
+  }, [isLoggedIn, activeTab]);
 
   return (
     <main>
@@ -55,27 +80,40 @@ const MyActivity = () => {
       </div>
       <nav>
         <ul>
-          <li>내가 만든 퀴즈</li>
+          <li onClick={() => setActiveTab('quizzes')}>내가 만든 퀴즈</li>
+          <li onClick={() => setActiveTab('posts')}>내가 쓴 글</li>
         </ul>
       </nav>
-      <table>
-        <thead>
-          <tr>
-            <th>제목</th>
-            <th>점수</th>
-            <th>날짜</th>
-          </tr>
-        </thead>
-        <tbody>
-          {quizzes.map((quiz, index) => (
-            <tr key={index}>
-              <td>{quiz.title}</td>
-              <td>점수</td>
-              <td>{formatToLocaleDateTimeString(quiz.created_at)}</td>
+      {activeTab === 'quizzes' && (
+        <table>
+          <thead>
+            <tr>
+              <th>제목</th>
+              <th>점수</th>
+              <th>날짜</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {quizzes.map((quiz, index) => (
+              <tr key={index}>
+                <td>{quiz.title}</td>
+                <td>점수</td>
+                <td>{formatToLocaleDateTimeString(quiz.created_at)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {activeTab === 'posts' && (
+        <div>
+         {myPosts.map((post, index) => (
+              <tr key={index}>
+                <td>{post.title}</td>
+                <td>{formatToLocaleDateTimeString(post.created_at)}</td>
+              </tr>
+            ))}
+        </div>
+      )}
     </main>
   );
 };
