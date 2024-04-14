8de3dd91d98986ca78c304d846f973f8d00ee4a0
kimsohyeon <aotoyae@gmail.com>
2024-04-15 02:36:28 +0900

    [style] 배너 blue, light blue 랜덤으로 설정

--- src/app/(main)/(components)/Banner.tsx
+++ src/app/(main)/(components)/Banner.tsx
@@ -2,16 +2,18 @@ import Image from 'next/image';
 import LogoVertical3 from '@/assets/logo_vertical_3.png';
 
 const Banner = () => {
+  const banners = ['banner_v2_b.png', 'banner_v2_lb.png'];
+  const randomIdx = Math.floor(Math.random() * banners.length);
+
   return (
-    <main className="w-full h-[400px] border-solid border-b-2 border-pointColor1">
-      <div className="pt-[80px] flex items-center justify-center gap-5">
-        <Image src={LogoVertical3} alt="로고" width={150} quality={100} />
-        <section className="text-4xl font-bold text-pointColor1">
-          지금 이 한국어, 뭔말인지 아는 그날까지!
-          <br />
-          <span className="text-2xl font-normal text-black">다함께 만들어 공유하는 재미있는 한국어 퀴즈 세상</span>
-        </section>
-      </div>
+    <main className="w-full border-solid border-b-2 border-pointColor1">
+      <Image
+        src={`https://icnlbuaakhminucvvzcj.supabase.co/storage/v1/object/public/assets/${banners[randomIdx]}`}
+        alt="로고"
+        width={1920}
+        height={600}
+        quality={100}
+      />
     </main>
   );
 };
