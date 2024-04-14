026a2245807978f99cc339593b52bf80329f3231
C1oudys <hymin310@gmail.com>
2024-04-14 21:16:21 +0900

    [feat] 어드민 페이지 신고글 보러가기 구현

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -24,3 +24,27 @@ export const insertReport = async (report: Report) => {
     throw error;
   }
 };
+
+export const getQuizzesReports = async (): Promise<Admin[]> => {
+  try {
+    const { data, error } = await supabase.from('admin').select('*').eq('type', 'quizzes');
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error('신고 데이터 받아오기 실패', error);
+    alert('일시적으로 신고 데이터를 받아오지 못했습니다. 다시 시도하세요.');
+    throw error;
+  }
+};
+
+export const getPostsReports = async (): Promise<Admin[]> => {
+  try {
+    const { data, error } = await supabase.from('admin').select('*').eq('type', 'posts');
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error('신고 데이터 받아오기 실패', error);
+    alert('일시적으로 신고 데이터를 받아오지 못했습니다. 다시 시도하세요.');
+    throw error;
+  }
+};
--- src/types/admin.ts
+++ src/types/admin.ts
@@ -1,6 +1,9 @@
 export type Admin = {
+  id?: string;
   type: string;
   title: string;
+  created_at?: string;
+  status?: boolean;
   target_id: string | string[];
   reported_user_id: string;
 };
