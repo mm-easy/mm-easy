35e2cf783d8e6ed20aea833c74da58ea533f3a6e
porosadporosad <tmxk1594@naver.com>
2024-04-16 22:40:39 +0900

    [test]test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ const config: Config = {
         pointColor1: '#2B84ED',
         pointColor2: '#FF8878',
         pointColor3: '#D9D9D9',
-        pointColor4: '#8dbdf6',
-        blackColor: '#2e2e2e'
+        blackColor: '#2E2E2E',
+        grayColor: '#D9D9D9'
       }
     },
     backgroundSize: { md: '80%', cover: 'cover' }
