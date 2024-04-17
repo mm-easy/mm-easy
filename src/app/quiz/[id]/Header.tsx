a6724924809628e35d2448742b462b9e8c1fac3e
kimsohyeon <aotoyae@gmail.com>
2024-04-17 16:48:20 +0900

    [feat] 퀴즈 풀기 나가기 버튼 추가

--- src/app/quiz/[id]/Header.tsx
+++ src/app/quiz/[id]/Header.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 const Header = ({ level, title }: { level: number; title: string }) => {
+  const exitBtnWidth = Math.floor(window.innerHeight * 0.08);
+  console.log(exitBtnWidth);
+
   return (
-    <header className="h-[8vh] flex leading-[7.5vh] border-solid border-b-2 border-pointColor1">
+    <header className="h-[8vh] reslative flex leading-[7.5vh] border-solid border-b-2 border-pointColor1">
       <h2 className="w-[calc(16%+2px)] text-center font-bold text-pointColor1 bg-bgColor1 border-solid border-r-2 border-pointColor1">
         퀴즈 풀기
       </h2>
@@ -14,6 +19,11 @@ const Header = ({ level, title }: { level: number; title: string }) => {
         제목
       </h2>
       <h3 className="pl-[2%]">{title}</h3>
+      <button
+        className={`w-[${exitBtnWidth}px] h-[calc(8vh-2px)] absolute right-0 bg-bgColor1 border-l-2 border-solid border-pointColor1`}
+      >
+        ✕
+      </button>
     </header>
   );
 };
