b275467857b74d0061bbf0c7cd3967222191f5ac
redberry0217 <redberry0217@gmail.com>
2024-04-19 02:14:37 +0900

    [feat]다국어 기능 라이브러리 테스트중

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,19 +3,23 @@
 import Link from 'next/link';
 import MainLogo from '@/assets/logo_horizontal_1.png';
 import Image from 'next/image';
+import ProfileDropdown from './ProfileDropdown';
 import { useEffect } from 'react';
 import { useAtom } from 'jotai';
 import { useState } from 'react';
 import { AuthChangeEvent } from '@supabase/supabase-js';
 import { isLoggedInAtom } from '../../store/store';
 import { supabase } from '@/utils/supabase/supabase';
-import ProfileDropdown from './ProfileDropdown';
 import { usePathname } from 'next/navigation';
+import { GetStaticPropsContext } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
 
 const Header = () => {
   const pathname = usePathname();
   const [isLoggedIn, setIsLoggedIn] = useAtom(isLoggedInAtom);
   const [currentUserEmail, setCurrentUserEmail] = useState<string>();
+  const { t } = useTranslation();
 
   /** 현재 로그인되어 있는지 확인 */
   useEffect(() => {
@@ -82,20 +86,45 @@ const Header = () => {
       </Link>
       <section className="w-[84%] flex justify-between px-10">
         <nav className="flex gap-14">
-          <Link href="/quiz/list" className={`hover:border-b-5 hover:border-solid hover:border-pointColor1 ${isActive('/quiz') ? 'border-b-5 border-solid border-pointColor1' : ''}`}>
-            퀴즈
+          <Link
+            href="/quiz/list"
+            className={`hover:border-b-5 hover:border-solid hover:border-pointColor1 ${
+              isActive('/quiz') ? 'border-b-5 border-solid border-pointColor1' : ''
+            }`}
+          >
+            {t('header:퀴즈')}
           </Link>
-          <Link href="/typing-game" className={`hover:border-b-5 hover:border-solid hover:border-pointColor1 ${isActive('/typing-game') ? 'border-b-5 border-solid border-pointColor1' : ''}`}>
+          <Link
+            href="/typing-game"
+            className={`hover:border-b-5 hover:border-solid hover:border-pointColor1 ${
+              isActive('/typing-game') ? 'border-b-5 border-solid border-pointColor1' : ''
+            }`}
+          >
             타자 연습
           </Link>
-          <Link href="/community/list/전체" className={`hover:border-b-5 hover:border-solid hover:border-pointColor1 ${isActive('/community') ? 'border-b-5 border-solid border-pointColor1' : ''}`}>
+          <Link
+            href="/community/list/전체"
+            className={`hover:border-b-5 hover:border-solid hover:border-pointColor1 ${
+              isActive('/community') ? 'border-b-5 border-solid border-pointColor1' : ''
+            }`}
+          >
             커뮤니티
           </Link>
-          <Link href="/about" className={`hover:border-b-5 hover:border-solid hover:border-pointColor1 ${isActive('/about') ? 'border-b-5 border-solid border-pointColor1' : ''}`}>
+          <Link
+            href="/about"
+            className={`hover:border-b-5 hover:border-solid hover:border-pointColor1 ${
+              isActive('/about') ? 'border-b-5 border-solid border-pointColor1' : ''
+            }`}
+          >
             서비스 소개
           </Link>
           {currentUserEmail === 'daejang@mmeasy.com' && (
-            <Link href="/admin" className={`text-pointColor2 hover:border-b-5 hover:border-solid hover:border-pointColor1 ${isActive('/admin') ? 'border-b-5 border-solid border-pointColor1' : ''}`}>
+            <Link
+              href="/admin"
+              className={`text-pointColor2 hover:border-b-5 hover:border-solid hover:border-pointColor1 ${
+                isActive('/admin') ? 'border-b-5 border-solid border-pointColor1' : ''
+              }`}
+            >
               관리자
             </Link>
           )}
