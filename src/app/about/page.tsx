67ee9fe982c10be12f082fd5b1659ae87153e0a4
kimsohyeon <aotoyae@gmail.com>
2024-04-18 17:49:32 +0900

    [style] 기능 소개 부분 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -83,43 +83,32 @@ const AboutPage = () => {
   };
 
   return (
-    <div className="flex flex-col text-center items-center">
-      <div className="flex flex-col justify-center items-center bg-pointColor1 w-full">
-        <div className="w-full">
-          <h2 className="text-5xl font-extrabold pb-16 text-white inline-block py-36">재밌게 배우는 한국어!</h2>
-        </div>
-        <div className="flex justify-center w-3/5 items-center">
-          <Image src={Infopeople} alt="로고" width={900} quality={100} />
-        </div>
-        <div className="text-3xl w-full text-white ">
-          <div>
-            <h2 className="py-16 text-center inline-block w-3/5">
-              <div className="flex justify-center items-center gap-4">
-                <Image src={InfoIcon3} alt="로고" width={50} />
-                <span className="ml-2 text-white">직접 퀴즈를 만들면서 한국어 실력을 키워보세요!</span>
-              </div>
-            </h2>
-          </div>
-          <div>
-            <h2 className="py-16 text-center inline-block border-t border-solid border-white w-3/5">
-              <div className="flex justify-center items-center gap-4">
-                <Image src={InfoIcon2} alt="로고" width={50} />
-                <span className="ml-2 text-white">타자 연습으로 한국어에 익숙해져 보세요!</span>
-              </div>
-            </h2>
-          </div>
-          <div>
-            <h2 className="pt-16 pb-24 text-center inline-block border-t border-solid border-white w-3/5">
-              <div className="flex justify-center items-center gap-4">
-                <Image src={InfoIcon1} alt="로고" width={50} />
-                <span className="ml-2 text-white">커뮤니티에서 소통하며 자신감을 길러보세요!</span>
-              </div>
-            </h2>
-          </div>
-        </div>
-      </div>
-
-      <div className="border-b-2 border-solid border-pointColor1 w-full bg-sky-50">
+    <main className="flex flex-col items-center">
+      <article className="w-full py-24 flex flex-col justify-center items-center bg-pointColor1 text-center">
+        <h2 className="text-5xl pb-16 font-black text-white tracking-wider">재밌게 배우는 한국어!</h2>
+        <Image src={Infopeople} alt="로고" width={800} quality={100} />
+        <section className="text-3xl w-full text-white ">
+          <section>
+            <div className="py-16 flex justify-center items-center gap-6">
+              <Image src={InfoIcon3} alt="로고" width={50} />
+              <span className="text-white">직접 퀴즈를 만들면서 한국어 실력을 키워보세요!</span>
+            </div>
+          </section>
+          <section>
+            <div className="w-3/5 mx-auto py-16 flex justify-center items-center gap-6 border-t border-solid border-white">
+              <Image src={InfoIcon2} alt="로고" width={50} />
+              <span className="text-white">타자 연습으로 한국어에 익숙해져 보세요!</span>
+            </div>
+          </section>
+          <section>
+            <div className="w-3/5 mx-auto pt-16 flex justify-center items-center gap-6 border-t border-solid border-white">
+              <Image src={InfoIcon1} alt="로고" width={50} />
+              <span className="text-white">커뮤니티에서 소통하며 자신감을 길러보세요!</span>
+            </div>
+          </section>
+        </section>
+      </article>
+      <article className="border-b-2 border-solid border-pointColor1 w-full bg-sky-50">
         <h2 className="py-12 text-4xl font-extrabold text-pointColor1">
           <span style={{ display: 'inline-flex', alignItems: 'center' }}>
             <Image src={LogoHorizontal1} alt="로고" width={200} quality={100} />
@@ -144,13 +133,13 @@ const AboutPage = () => {
             );
           })}
         </div>
-      </div>
-      <div className="flex flex-col text-xl font-bold w-3/6 pb-20">
+      </article>
+      <article className="flex flex-col text-xl font-bold w-3/6 pb-20">
         <h2 className="text-4xl font-extrabold py-16 text-pointColor1">유저후기</h2>
         <div className="flex flex-col flex-wrap">
           <div className="flex justify-center items-center w-full pl-10 relative z-10">
             <div className="flex flex-col ml-auto mb-6 w-2/3">
-              <div className="about-userreview1 bg-white rounded-lg p-4 border border-solid border-pointColor1">
+              <div className="about-userreview1 relative bg-white rounded-lg p-4 border border-solid border-pointColor1">
                 <span className="">&quot;한국어 속도가 확 늘었어요~ 모두 타자 연습 게임 덕분!&quot;</span>
                 <span className="text-pointColor1 text-base block mt-2">Elisa, 멕시코</span>
               </div>
@@ -191,8 +180,8 @@ const AboutPage = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="w-full mt-20 border-t-2 border-solid border-pointColor1 bg-bgColor1">
+      </article>
+      <article className="w-full mt-20 border-t-2 border-solid border-pointColor1 bg-bgColor1">
         <h2 className="text-pointColor1 text-4xl font-extrabold pt-16 pb-10">뭔말이지? 프로젝트에 참여한 사람들</h2>
         <h2 className="text-pointColor1 text-xl font-bold pb-24">Team Coding Zizon</h2>
 
@@ -240,8 +229,8 @@ const AboutPage = () => {
             );
           })}
         </div>
-      </div>
-    </div>
+      </article>
+    </main>
   );
 };
 
