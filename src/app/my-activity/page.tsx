52031ddcd4702046ae27d49f3634201d6c204f0a
Seoweol <ahddl622@naver.com>
2024-04-19 12:22:55 +0900

    [test] test

--- src/app/my-activity/page.tsx
+++ src/app/my-activity/page.tsx
@@ -2,7 +2,7 @@ import MyActivity from "./MyActivity";
 
 const MyActivityPage = () => {
   return (
-    <div className="overflow-hidden">
+    <div className="no-scrollbar">
       <MyActivity />
     </div>
   );
