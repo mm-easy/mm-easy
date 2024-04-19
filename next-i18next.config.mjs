a85789f6c1a9956ffe4ac4053b756682a56ab555
redberry0217 <redberry0217@gmail.com>
2024-04-19 12:47:42 +0900

    [fix] quiz data invalidate 수정, 로그 삭제 등

--- next-i18next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-i18next').UserConfig} */
-export const i18n = {
-  defaultLocale: 'ko',
-  locales: ['ko', 'en'],
-
-  /** true로 설정 시 Next.js에서 자동으로 접속한 사용자의 로케일로 표시함 */
-  localeDetection: false
-};
