10a68880c5bcbe1f71d883bded71daaee7c8d5d5
kimsohyeon <aotoyae@gmail.com>
2024-04-22 01:56:14 +0900

    [delete] lang 넘겨주는 곳들 삭제

--- src/components/common/DeleteButton.tsx
+++ src/components/common/DeleteButton.tsx
@@ -5,12 +5,9 @@ import { FormCommentButtonProps, FormPostButtonProps } from '@/types/posts';
 import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
-import { langAtom } from '@/store/store';
-import { useAtom } from 'jotai';
 
 export const PostDeleteButton: React.FC<FormPostButtonProps> = ({ text, width, height, postId, redirectUrl }) => {
-  const [lang] = useAtom(langAtom);
-  const m = useMultilingual(lang, 'communityPost');
+  const m = useMultilingual('communityPost');
   const queryClient = useQueryClient();
   const router = useRouter();
 
@@ -18,7 +15,7 @@ export const PostDeleteButton: React.FC<FormPostButtonProps> = ({ text, width, h
     if (window.confirm(m('COMMUNITY_POST_DELETE'))) {
       try {
         await removeCommunityPost(postId);
-        toast(m('COMMUNITY_POST_DELETE_COMPLETE'))
+        toast(m('COMMUNITY_POST_DELETE_COMPLETE'));
         if (redirectUrl) {
           router.replace(redirectUrl);
         }
@@ -48,7 +45,7 @@ export const CommentDeleteBtn: React.FC<FormCommentButtonProps> = ({ text, width
     if (window.confirm('해당 댓글을 삭제하시겠습니까?')) {
       try {
         await handleDeleteBtn(userId);
-        toast("댓글이 삭제되었습니다.")
+        toast('댓글이 삭제되었습니다.');
         if (redirectUrl) {
           router.replace(redirectUrl);
         }
