720c5e2f2cb9bd32fba9aee0540a40e1145114b1
kimsohyeon <aotoyae@gmail.com>
2024-04-21 14:32:42 +0900

    [feat] 나의 프로필 번역

---
+++ src/constant/locales/my-page/my-profile.ts
@@ -0,0 +1,64 @@
+const MY_PROFILE_STRINGS = {
+  HEADER: {
+    ko: '프로필',
+    en: 'Profile'
+  },
+  NICKNAME: {
+    ko: '닉네임',
+    en: 'Nickname'
+  },
+  EMAIL: {
+    ko: '이메일',
+    en: 'Email'
+  },
+  PROFILE_EDIT: {
+    ko: '프로필 수정',
+    en: 'To Edit a Profile'
+  },
+  PROFILE_EDIT_HEADER: {
+    ko: '프로필 수정',
+    en: 'Edit a profile'
+  },
+  CANCEL_BTN: {
+    ko: '취소하기',
+    en: 'CANCEL'
+  },
+  COMPLETE_BTN: {
+    ko: '수정완료',
+    en: 'COMPLETE'
+  },
+  MY_QUIZ_INFO_TEXT1: {
+    ko: '전체 올라온 퀴즈 ',
+    en: "You've solved "
+  },
+  MY_QUIZ_INFO_TEXT2: {
+    ko: ' 개 중 ',
+    en: ' out of the '
+  },
+  MY_QUIZ_INFO_TEXT3: {
+    ko: ' 개의 퀴즈를 풀었어요!',
+    en: ' quizzes posted so far!'
+  },
+  LEVEL: {
+    ko: '레벨',
+    en: 'Level'
+  },
+  QUIZ_SCORE: {
+    ko: '퀴즈 점수',
+    en: 'Quiz Score'
+  },
+  TYPING_GAME_SCORE: {
+    ko: '타자 연습 점수',
+    en: 'Typing Practice Score'
+  },
+  TOTAL_SCORE: {
+    ko: '총 점수',
+    en: 'Total Score'
+  },
+  TO_MY_ACTIVITY: {
+    ko: '나의 활동 보러기',
+    en: 'To My Activity'
+  }
+};
+
+export default MY_PROFILE_STRINGS;
--- src/utils/useMultilingual.ts
+++ src/utils/useMultilingual.ts
@@ -4,6 +4,7 @@ import TYPING_GAME_STRINGS from '@/constant/locales/typing-game/typing-game';
 import COMMUNITY_SECTION_STRINGS from '@/constant/locales/home/community-section';
 import QUIZ_SECTION_STRINGS from '@/constant/locales/home/quiz-section';
 import RANKING_SECTION_STRINGS from '@/constant/locales/home/ranking-section';
+import MY_PROFILE_STRINGS from '@/constant/locales/my-page/my-profile';
 
 export type LanguageType = 'ko' | 'en';
 
@@ -24,6 +25,8 @@ export default function useMultilingual(lang: LanguageType, assets: string) {
     assetsObject = QUIZ_SECTION_STRINGS;
   } else if (assets === 'ranking-section') {
     assetsObject = RANKING_SECTION_STRINGS;
+  } else if (assets === 'my-profile') {
+    assetsObject = MY_PROFILE_STRINGS;
   }
   return (key: keyof typeof assetsObject) => {
     return assetsObject[key][lang];
