00697beca594ac765f78406bffd74000c2f93acc
Seoweol <ahddl622@naver.com>
2024-04-23 19:48:21 +0900

    [style] 커뮤니티 반응형

--- src/app/community/list/[category]/CategorySelector.tsx
+++ src/app/community/list/[category]/CategorySelector.tsx
@@ -45,7 +45,7 @@ const CategorySelector = ({ categoryNow }: { categoryNow: string | null }) => {
         {Object.keys(categoryMenu).map((category) => (
           <li
             key={category}
-            className={`h-[8vh] sm:w-full flex items-center pl-12 border-b-2 border-solid border-pointColor1 cursor-pointer ${
+            className={`h-[8vh] sm:w-[] flex items-center pl-12 border-b-2 border-solid border-pointColor1 cursor-pointer ${
               categoryNow === categoryMenu[category] ? 'bg-pointColor1 text-white' : 'bg-white'
             }`}
             onClick={() => handleSelectCategory(category)}
