546cf342997d5f8213e73b89220875230e8b8098
C1oudys <hymin310@gmail.com>
2024-04-24 15:28:55 +0900

    [fix] sm 사용 수정

--- src/app/(main)/(components)/CommunitySection.tsx
+++ src/app/(main)/(components)/CommunitySection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import LoadingImg from '@/components/common/LoadingImg';
 import useMultilingual from '@/utils/useMultilingual';
@@ -8,7 +8,6 @@ import { useQuery } from '@tanstack/react-query';
 
 const CommunitySection = () => {
   const m = useMultilingual('community-section');
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
   const [activeTab, setActiveTab] = useState('notice');
 
   const { data: posts, isLoading: postsLoading } = useQuery({
@@ -23,17 +22,6 @@ const CommunitySection = () => {
     staleTime: 1000 * 60 * 5
   });
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 580);
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize();
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   if (postsLoading || noticesLoading) {
     return <LoadingImg height="400px" />;
   }
@@ -43,114 +31,111 @@ const CommunitySection = () => {
       <div className="w-full px-6 py-4 text-lg font-bold text-pointColor1 bg-bgColor1 border-y-2 border-solid border-pointColor1 sm:hidden">
         <span className="sm:text-xl">{m('RECENT_POSTS')}</span>
       </div>
-      {isSmallScreen ? (
-        <div className="">
-          <div className="w-full px-6 py-4 text-lg font-bold text-pointColor1 bg-bgColor1 flex justify-between border-t-2 border-solid border-pointColor1">
-            <span className="sm:text-xl">{m('RECENT_POSTS')}</span>
-            <div className="flex">
-              <button
-                onClick={() => setActiveTab('notice')}
-                className={`mr-2 px-2 font-bold text-sm border-2 border-solid border-pointColor1 rounded-full ${
-                  activeTab === 'notice' ? 'bg-pointColor1 text-white' : 'bg-white text-pointColor1'
-                }`}
-              >
-                {m('NOTICE_BUTTON')}
-              </button>
-              <button
-                onClick={() => setActiveTab('posts')}
-                className={`px-2 font-bold text-sm border-2 border-solid border-pointColor1 rounded-full ${
-                  activeTab === 'posts' ? 'bg-pointColor1 text-white' : 'bg-white text-pointColor1'
-                }`}
-              >
-                {m('USER_POSTS_BUTTON')}
-              </button>
-            </div>
+      <div className="hidden sm:block">
+        <div className="w-full px-6 py-4 text-lg font-bold text-pointColor1 bg-bgColor1 flex justify-between border-t-2 border-solid border-pointColor1">
+          <span className="sm:text-xl">{m('RECENT_POSTS')}</span>
+          <div className="flex">
+            <button
+              onClick={() => setActiveTab('notice')}
+              className={`mr-2 px-2 font-bold text-sm border-2 border-solid border-pointColor1 rounded-full ${
+                activeTab === 'notice' ? 'bg-pointColor1 text-white' : 'bg-white text-pointColor1'
+              }`}
+            >
+              {m('NOTICE_BUTTON')}
+            </button>
+            <button
+              onClick={() => setActiveTab('posts')}
+              className={`px-2 font-bold text-sm border-2 border-solid border-pointColor1 rounded-full ${
+                activeTab === 'posts' ? 'bg-pointColor1 text-white' : 'bg-white text-pointColor1'
+              }`}
+            >
+              {m('USER_POSTS_BUTTON')}
+            </button>
           </div>
-          {activeTab === 'notice' && (
-            <div className="p-4 bg-bgColor1 border-solid border-b-2 border-pointColor1">
-              {notices?.map((notice, index) => (
-                <div
-                  key={notice.id}
-                  className={`py-4 ${index !== notices.length - 1 && 'border-b border-solid border-pointColor1'}`}
-                >
-                  <Link href={`/community/list/전체/${notice.id}`} className="flex flex-col gap-2">
-                    <h2 className="text-lg font-bold truncate">{notice.title}</h2>
-                    <time>
-                      {m('DATE_CREATION')}: {formatToLocaleDateTimeString(notice.created_at)}
-                    </time>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          )}
-          {activeTab === 'posts' && (
-            <div className="p-4 bg-bgColor1 border-solid border-b-2 border-pointColor1">
-              {posts?.map((post, index) => (
-                <div
-                  key={post.id}
-                  className={`py-4 ${index !== posts.length - 1 && 'border-b border-solid border-pointColor1'}`}
-                >
-                  <Link href={`/community/list/전체/${post.id}`} className="flex flex-col gap-2">
-                    <h2 className="text-lg font-bold truncate">{post.title}</h2>
-                    <time>
-                      {m('DATE_CREATION')}: {formatToLocaleDateTimeString(post.created_at)}
-                    </time>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          )}
         </div>
-      ) : (
-        <section className="flex">
-          <div className="w-1/2 p-8 border-r border-solid border-pointColor1">
-            <div className="flex justify-between">
-              <h2 className="mb-4 text-lg font-bold">{m('NOTICE')}</h2>
-              <Link href={`/community/list/공지`} className="font-semibold text-pointColor1">
-                {m('MORE')}
-              </Link>
-            </div>
-            <div>
-              {notices?.map((notice, index) => (
-                <div
-                  key={notice.id}
-                  className={`py-4 ${index !== notices.length - 1 && 'border-b border-solid border-pointColor1'}`}
-                >
-                  <Link href={`/community/list/전체/${notice.id}`} className="flex flex-col gap-2">
-                    <h2 className="text-lg font-bold truncate">{notice.title}</h2>
-                    <time>
-                      {m('DATE_CREATION')}: {formatToLocaleDateTimeString(notice.created_at)}
-                    </time>
-                  </Link>
-                </div>
-              ))}
-            </div>
+        {activeTab === 'notice' && (
+          <div className="p-4 bg-bgColor1 border-solid border-b-2 border-pointColor1">
+            {notices?.map((notice, index) => (
+              <div
+                key={notice.id}
+                className={`py-4 ${index !== notices.length - 1 && 'border-b border-solid border-pointColor1'}`}
+              >
+                <Link href={`/community/list/전체/${notice.id}`} className="flex flex-col gap-2">
+                  <h2 className="text-lg font-bold truncate">{notice.title}</h2>
+                  <time>
+                    {m('DATE_CREATION')}: {formatToLocaleDateTimeString(notice.created_at)}
+                  </time>
+                </Link>
+              </div>
+            ))}
           </div>
-          <div className="w-1/2 p-8">
-            <div className="flex justify-between">
-              <h2 className="mb-4 text-lg font-bold">{m('USER_POSTS')}</h2>
-              <Link href={`/community/list/전체`} className="font-semibold text-pointColor1">
-                {m('MORE')}
-              </Link>
-            </div>
-            <div>
-              {posts?.map((post, index) => (
-                <div
-                  key={post.id}
-                  className={`py-4 ${index !== posts.length - 1 && 'border-b border-solid border-pointColor1'}`}
-                >
-                  <Link href={`/community/list/전체/${post.id}`} className="flex flex-col gap-2">
-                    <h2 className="text-lg font-bold truncate">{post.title}</h2>
-                    <time>
-                      {m('DATE_CREATION')}: {formatToLocaleDateTimeString(post.created_at)}
-                    </time>
-                  </Link>
-                </div>
-              ))}
-            </div>
+        )}
+        {activeTab === 'posts' && (
+          <div className="p-4 bg-bgColor1 border-solid border-b-2 border-pointColor1">
+            {posts?.map((post, index) => (
+              <div
+                key={post.id}
+                className={`py-4 ${index !== posts.length - 1 && 'border-b border-solid border-pointColor1'}`}
+              >
+                <Link href={`/community/list/전체/${post.id}`} className="flex flex-col gap-2">
+                  <h2 className="text-lg font-bold truncate">{post.title}</h2>
+                  <time>
+                    {m('DATE_CREATION')}: {formatToLocaleDateTimeString(post.created_at)}
+                  </time>
+                </Link>
+              </div>
+            ))}
           </div>
-        </section>
-      )}
+        )}
+      </div>
+      <section className="flex flex:block sm:hidden">
+        <div className="w-1/2 p-8 border-r border-solid border-pointColor1">
+          <div className="flex justify-between">
+            <h2 className="mb-4 text-lg font-bold">{m('NOTICE')}</h2>
+            <Link href={`/community/list/공지`} className="font-semibold text-pointColor1">
+              {m('MORE')}
+            </Link>
+          </div>
+          <div>
+            {notices?.map((notice, index) => (
+              <div
+                key={notice.id}
+                className={`py-4 ${index !== notices.length - 1 && 'border-b border-solid border-pointColor1'}`}
+              >
+                <Link href={`/community/list/전체/${notice.id}`} className="flex flex-col gap-2">
+                  <h2 className="text-lg font-bold truncate">{notice.title}</h2>
+                  <time>
+                    {m('DATE_CREATION')}: {formatToLocaleDateTimeString(notice.created_at)}
+                  </time>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-1/2 p-8">
+          <div className="flex justify-between">
+            <h2 className="mb-4 text-lg font-bold">{m('USER_POSTS')}</h2>
+            <Link href={`/community/list/전체`} className="font-semibold text-pointColor1">
+              {m('MORE')}
+            </Link>
+          </div>
+          <div>
+            {posts?.map((post, index) => (
+              <div
+                key={post.id}
+                className={`py-4 ${index !== posts.length - 1 && 'border-b border-solid border-pointColor1'}`}
+              >
+                <Link href={`/community/list/전체/${post.id}`} className="flex flex-col gap-2">
+                  <h2 className="text-lg font-bold truncate">{post.title}</h2>
+                  <time>
+                    {m('DATE_CREATION')}: {formatToLocaleDateTimeString(post.created_at)}
+                  </time>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/app/(main)/(components)/QuizSection.tsx
+++ src/app/(main)/(components)/QuizSection.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import QuestionEx from './QuestionEx';
@@ -20,24 +17,12 @@ import type { Quiz } from '@/types/quizzes';
 
 const QuizSection = () => {
   const m = useMultilingual('quiz-section');
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   const { data: quiz, isLoading } = useQuery<Quiz[]>({
     queryKey: ['recentQuiz'],
     queryFn: getRecentQuizzes,
     staleTime: 1000 * 60 * 5
   });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 580);
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize();
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   if (isLoading) {
     return <LoadingImg height="400px" />;
@@ -51,7 +36,7 @@ const QuizSection = () => {
           {m('MORE')}
         </Link>
       </div>
-      {isSmallScreen ? (
+      <div className="hidden sm:block">
         <Swiper
           modules={[Pagination, Navigation]}
           spaceBetween={5}
@@ -79,32 +64,31 @@ const QuizSection = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-      ) : (
-        <section className="px-6 py-4 grid grid-cols-4 gap-5">
-          {quiz?.map((quiz) => (
-            <div
-              key={quiz.id}
-              className="p-4 flex flex-col my-5 border border-solid border-grayColor1 rounded-t-3xl rounded-b-md transition duration-300 ease-in-out transform hover:border-blue-500"
-            >
-              <p className="font-bold text-lg mt-4 mb-3 truncate">{quiz.title}</p>
-              <div className="flex flex-col gap-3">
-                <Image
-                  src={`${storageUrl}/quiz-thumbnails/${quiz.thumbnail_img_url}`}
-                  alt="퀴즈 썸네일"
-                  width={250}
-                  height={250}
-                  quality={100}
-                  className="w-full h-[250px] object-cover border border-solid border-grayColor1 rounded-md"
-                />
-                <QuestionEx id={quiz.id} />
-                <Link href={`/quiz/${quiz.id}`}>
-                  <div className="p-2 text-center text-white bg-pointColor1 rounded-md">{m('TAKE_THE_QUIZ')}</div>
-                </Link>
-              </div>
+      </div>
+      <section className="px-6 py-4 grid grid-cols-4 gap-5 flex:block sm:hidden">
+        {quiz?.map((quiz) => (
+          <div
+            key={quiz.id}
+            className="p-4 flex flex-col my-5 border border-solid border-grayColor1 rounded-t-3xl rounded-b-md transition duration-300 ease-in-out transform hover:border-blue-500"
+          >
+            <p className="font-bold text-lg mt-4 mb-3 truncate">{quiz.title}</p>
+            <div className="flex flex-col gap-3">
+              <Image
+                src={`${storageUrl}/quiz-thumbnails/${quiz.thumbnail_img_url}`}
+                alt="퀴즈 썸네일"
+                width={250}
+                height={250}
+                quality={100}
+                className="w-full h-[250px] object-cover border border-solid border-grayColor1 rounded-md"
+              />
+              <QuestionEx id={quiz.id} />
+              <Link href={`/quiz/${quiz.id}`}>
+                <div className="p-2 text-center text-white bg-pointColor1 rounded-md">{m('TAKE_THE_QUIZ')}</div>
+              </Link>
             </div>
-          ))}
-        </section>
-      )}
+          </div>
+        ))}
+      </section>
     </>
   );
 };
