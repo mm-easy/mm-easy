ed2e60ed6e8b00f769f959b7864854f2a39445f0
kimsohyeon <aotoyae@gmail.com>
2024-04-24 21:23:31 +0900

    [feat] 페이지에 따라 아이콘 컬러 변경

--- src/components/layout/MobileMenu.tsx
+++ src/components/layout/MobileMenu.tsx
@@ -1,8 +1,16 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import HomeIcon from '@/assets/mobile/mobile_btn_home.svg';
+import QuizIcon from '@/assets/mobile/mobile_btn_quiz.svg';
+import TypingIcon from '@/assets/mobile/mobile_btn_typing.svg';
+import PhonicsIcon from '@/assets/mobile/mobile_btn_phonics.svg';
+import CommunityIcon from '@/assets/mobile/mobile_btn_community.svg';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const MobileMenu = () => {
+  const pathname = usePathname();
   const [isMenuVisible, setIsMenuVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPost] = useState(0);
 
@@ -21,14 +29,42 @@ const MobileMenu = () => {
     };
   }, [prevScrollPos]);
 
+  const isActive = (path: string) => {
+    if (path === '/') return pathname === path;
+    return pathname.includes(path);
+  };
+
   return (
-    <nav className="hidden sm:block">
-      <ul
-        className={`w-full h-[8vh] bottom-0 fixed z-10 bg-bgColor1 transition-opacity duration-300 ${isMenuVisible ? 'opacity-100' : 'opacity-0 hidden'}`}
+    <section className="hidden sm:block">
+      <nav
+        className={`w-full h-[12vh] bottom-0 fixed z-10 bg-bgColor1 transition-opacity duration-300 ${isMenuVisible ? 'opacity-100' : 'opacity-0 hidden'} pr-2 grid grid-cols-5 items-center gap-2 border-t border-solid border-pointColor1`}
       >
-        <li></li>
-      </ul>
-    </nav>
+        <Link href="/" className="flex flex-col items-center gap-2 text-center">
+          <HomeIcon style={{ fill: isActive('/') ? '#2B84ED' : '#8dbdf6', width: '40px', height: '40px' }} />
+          <span className={`${isActive('/') ? 'text-pointColor1' : 'text-pointColor3'}`}>홈</span>
+        </Link>
+        <Link href="/quiz/list" className="flex flex-col items-center gap-2 text-center">
+          <QuizIcon style={{ fill: isActive('/quiz') ? '#2B84ED' : '#8dbdf6', width: '40px', height: '40px' }} />
+          <span className={`${isActive('/quiz') ? 'text-pointColor1' : 'text-pointColor3'}`}>퀴즈</span>
+        </Link>
+        <Link href="/typing-game" className="flex flex-col items-center gap-2 text-center">
+          <TypingIcon
+            style={{ fill: isActive('/typing-game') ? '#2B84ED' : '#8dbdf6', width: '40px', height: '40px' }}
+          />
+          <span className={`${isActive('/typing-game') ? 'text-pointColor1' : 'text-pointColor3'}`}>타자연습</span>
+        </Link>
+        <Link href="/phonics" className="flex flex-col items-center gap-2 text-center">
+          <PhonicsIcon style={{ fill: isActive('/phonics') ? '#2B84ED' : '#8dbdf6', width: '40px', height: '40px' }} />
+          <span className={`${isActive('/phonics') ? 'text-pointColor1' : 'text-pointColor3'}`}>한글공부</span>
+        </Link>
+        <Link href="/community/list/전체" className="flex flex-col items-center gap-2 text-center">
+          <CommunityIcon
+            style={{ fill: isActive('/community') ? '#2B84ED' : '#8dbdf6', width: '39px', height: '39px' }}
+          />
+          <span className={`${isActive('/community') ? 'text-pointColor1' : 'text-pointColor3'}`}>커뮤니티</span>
+        </Link>
+      </nav>
+    </section>
   );
 };
 
