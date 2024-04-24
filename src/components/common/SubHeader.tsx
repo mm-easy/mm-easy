5eadf4acf5540f851540339657c319ec90d5b3fa
redberry0217 <redberry0217@gmail.com>
2024-04-24 16:51:46 +0900

    [style] quiz-form 모바일 완료

--- src/components/common/SubHeader.tsx
+++ src/components/common/SubHeader.tsx
@@ -4,7 +4,7 @@ interface QuizFormHeaderProps {
 
 const SubHeader: React.FC<QuizFormHeaderProps> = ({ text }) => {
   return (
-    <main className="h-[8vh] font-bold pl-20 leading-[8vh] bg-bgColor1 border-b-2 border-pointColor1 text-pointColor1">
+    <main className="sm:hidden h-[8vh] font-bold pl-20 leading-[8vh] bg-bgColor1 border-b-2 border-pointColor1 text-pointColor1">
       {text}
     </main>
   );
