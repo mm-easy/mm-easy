fbda1b89972f68b835e7c4d639d5d692cfed438b
redberry0217 <redberry0217@gmail.com>
2024-04-27 22:38:17 +0900

    [feat] 파닉스 페이지 번역 완료

---
+++ src/constant/locales/hangul/vowels.ts
@@ -0,0 +1,52 @@
+const VOWELS_STRINGS = {
+  VOWEL_MENU1: {
+    ko: '기본 모음',
+    en: 'Basic Vowels'
+  },
+  VOWEL_MENU2: {
+    ko: '합자',
+    en: 'Combined Vowels'
+  },
+  VOWEL_TITLE: {
+    ko: '한글의 모음 글자는 21자이다. 이 중에서 10자가 기본 모음 글자이다.',
+    en: 'There are 21 vowel letters in Hangul. Among these, 10 are basic vowel letters.'
+  },
+  GUIDE_TEXT: {
+    ko: '글자를 눌러 발음을 들어보세요.',
+    en: 'Click on the letter to hear the pronunciation.'
+  },
+  LETTER_NAME: {
+    ko: '글자 이름',
+    en: `Letter's name`
+  },
+  B_VOWEL_INFO1: {
+    ko: '모음 글자를 처음 만들 때 ·, ㅡ, ㅣ의 형태를 기본으로 삼았다.',
+    en: 'When first creating vowel letters, the shapes of ·, ㅡ, and ㅣ were used as the basis.'
+  },
+  B_VOWEL_INFO2: {
+    ko: 'ㆍ는 하늘의 둥근 모양을, ㅡ는 땅의 평평한 모양을',
+    en: 'ㆍ imitates the round shape of the sky, ㅡ imitates the flat shape of the earth,'
+  },
+  B_VOWEL_INFO3: {
+    ko: 'ㅣ는 꼿꼿이 서 있는 사람의 모양을 본뜬 것이다.',
+    en: 'and ㅣ imitates the shape of a person standing upright.'
+  },
+  B_VOWEL_INFO4: {
+    ko: '이는 만물의 근본 요소를 하늘, 땅, 사람이라고 생각한 동양 철학을 적용한 것이다.',
+    en: 'This is an application of Eastern philosophy that considers the fundamental elements of all things to be heaven, earth, and people.'
+  },
+  B_VOWEL_INFO5: {
+    ko: '이 세 글자의 조합을 통해 기본 모음 글자 10자를 만들었다.',
+    en: 'By combining these three letters, 10 basic vowel letters were created.'
+  },
+  B_VOWEL_INFO6: {
+    ko: '·는 현재 쓰지 않는 글자이다.',
+    en: '· is a character that is not currently used.'
+  },
+  C_VOWEL_TITLE: {
+    ko: '기본 모음 글자의 조합으로 나머지 모음 글자를 만들었다.',
+    en: 'The remaining vowel letters were created by combining the basic vowel letters.'
+  }
+};
+
+export default VOWELS_STRINGS;
